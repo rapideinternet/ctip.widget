b6e29b3e9595db21f4ae35a4de4dce2df750fa0f
Rens Reinders <rens@rapide.nl>
2017-07-19 13:42:17 +0200

    search support

--- src/jquery.ctip.widget.js
+++ src/jquery.ctip.widget.js
@@ -59,7 +59,7 @@
 				// init and center map
 				var mapElem = $(this.element).find(this.settings.mapSelector)[0];
 				this._map = L.map(mapElem).setView([this.settings.center.lat, this.settings.center.lng], this.settings.center.zoom);
- 
+  
 				/*
 				L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 					attribution: "CTIP",
